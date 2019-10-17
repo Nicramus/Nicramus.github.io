2d2e349fc1cb89278dc605b0723d1e097de9aae2
Marcin Mielcarski <marcin.mielcarski@globallogic.com>
2019-10-17 07:40:44 +0200

    maps link added

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+/**
+ * sianowska custom javascript file
+ */
+
+ /**
+  * map initialization
+  */
+ function initMap() {
+    //52.4350337,16.810059
+    var sianowskaLocation = {lat: 52.4350337, lng: 16.810059};
+    var mapContainer = document.getElementById('map');
+    var map = new google.maps.Map(mapContainer, {zoom:4, center:sianowskaLocation});
+    var marker = new google.maps.Marker({position: sianowskaLocation, map: map});
+}
+
+initMap();
