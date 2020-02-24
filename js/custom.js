90f8bcfb8b550d5b4976e0710100c9dd8e17ae91
Marcin Mielcarski <larry.marcin@hotmail.com>
2020-02-24 21:11:22 +0100

    aaa

--- js/custom.js
+++ js/custom.js
@@ -15,9 +15,18 @@
     console.log("dupa");	
 	
 	const Http = new XMLHttpRequest();
+	
+	//Http.setRequestHeader('cache-control', 'no-cache, must-revalidate, post-check=0, pre-check=0');
+	//Http.setRequestHeader('cache-control', 'max-age=0');
+	//Http.setRequestHeader('expires', '0');
+	//Http.setRequestHeader('expires', 'Tue, 01 Jan 1980 1:00:00 GMT');
+	//Http.setRequestHeader('pragma', 'no-cache');
+	Http.setRequestHeader('cache-control', 'max-age=604800');
 	const url='https://jsonplaceholder.typicode.com/posts/1';
 	Http.open("GET", url);
 	Http.send();
+	
+	
 
 	Http.onreadystatechange = (e) => {
 	  console.log(Http.responseText)
